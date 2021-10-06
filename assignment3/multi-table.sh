e576edffe77d1d9d8c951d7b3028e5e3576936a4
chaehuiseon <smilechs34@gmail.com>
2021-10-07 05:16:44 +0900

    positive sign(plus) recognition

--- assignment3/multi-table.sh
+++ assignment3/multi-table.sh
@@ -5,12 +5,12 @@ then
 	echo "There is no input"
 	exit 1
 	
-elif [[ $1 -le 0 || ! $1 =~ ^[0-9]+$ ]]
+elif [[ $1 -le 0 || ! $1 =~ ^[-+]?[0-9]+$ ]]
 then
 	echo "The first argument input is wrong. Input condition is positive and integer."
 	exit 1
 	
-elif [[ ! $2 || $2 -le 0 || ! $2 =~ ^[0-9]+$ ]]
+elif [[ ! $2 || $2 -le 0 || ! $2 =~ ^[-+]?[0-9]+$ ]]
 then
 	echo "The second argument input is wrong. Input condition is positive and integer."
 	exit 1
